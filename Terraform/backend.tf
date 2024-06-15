b4b955541e7374afea4165b2d703661d32acba58
deepaksaikillada <kdsai2003@gmail.com>
2024-06-15 19:16:01 +0530

    deepak has pushed

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -3,6 +3,6 @@ terraform {
     storage_account_name = "upfstorage1"
     container_name       = "upfcontainer"
     key                  = "terraform.tfstate"
-    access_key           = "I3wJcMITYEDmMK8Nca2VoNI6PyIyQqj+mYX2fM5FV5cDSLdVWAeWcBlPtqCsOvLh4rBWeUn2PN5B+AStHKgsmg=="
+    //access_key           = "I3wJcMITYEDmMK8Nca2VoNI6PyIyQqj+mYX2fM5FV5cDSLdVWAeWcBlPtqCsOvLh4rBWeUn2PN5B+AStHKgsmg=="
   }
 }
